ca29a1242d096c9391190179a19e60fbb80e957c
Shok48 <145995576+Shok48@users.noreply.github.com>
2025-05-13 19:42:11 +0300

    Реализована базовая структура новигации и стилизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import '@ant-design/v5-patch-for-react-19';
+import { Header } from 'antd/es/layout/layout';
+import type { ReactNode } from 'react';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import styles from './App.module.css';
 
-function App() {
+interface IRouteConfig {
+    path: string,
+    label: string,
+    component: ReactNode | null,
+}
 
-  return (
-    <>
-      
-    </>
-  )
+const routes: IRouteConfig[] = [
+    {
+        path: '/',
+        label: 'Главная',
+        component: null,
+    }
+]
+
+const Navigation = () => (
+    <Header className={styles['header']}>
+        {routes.map(({path, label}) => (
+            <Link className={styles['link']} to={path} key={path}>{label}</Link>
+        ))}
+    </Header>
+)
+
+const App = () => {
+
+    return (
+        <Router>
+            <Navigation />
+            <Routes>
+                {routes.map(({path, component}) => (
+                    <Route path={path} element={component} key={path} />
+                ))}
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
